578c36ab412ee0c4b519e49683534b5b801e7895
Devodita Chakravarty <132634760+devodita@users.noreply.github.com>
2024-02-02 22:03:38 +0530

    Update HexPlane.py
    Using bicubic in place of bilinear

--- hexplane/model/HexPlane.py
+++ hexplane/model/HexPlane.py
@@ -368,7 +368,7 @@ class HexPlane(HexPlane_Base):
                 F.interpolate(
                     plane_coef[i].data,
                     size=(res_target[mat_id_1], res_target[mat_id_0]),
-                    mode="bilinear",
+                    mode="bicubic",
                     align_corners=self.align_corners,
                 )
             )
@@ -376,7 +376,7 @@ class HexPlane(HexPlane_Base):
                 F.interpolate(
                     line_time_coef[i].data,
                     size=(res_target[vec_id], time_grid),
-                    mode="bilinear",
+                    mode="bicubic",
                     align_corners=self.align_corners,
                 )
             )
